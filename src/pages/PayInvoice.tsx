e9f29efd72ef3582ffd61d48fa1bf224405325cf
Adityaakr <quant08@gmail.com>
2025-12-30 23:47:07 +0530

    Reduce PayInvoice page width from max-w-2xl to max-w-xl

--- src/pages/PayInvoice.tsx
+++ src/pages/PayInvoice.tsx
@@ -399,7 +399,7 @@ export default function PayInvoice() {
 
   return (
     <div className="min-h-screen bg-background flex items-center justify-center p-4">
-      <div className="mx-auto max-w-2xl w-full">
+      <div className="mx-auto max-w-xl w-full">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
